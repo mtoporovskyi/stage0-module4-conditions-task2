0650673f890c9beccef48e247b3af0194f6f85aa
Myroslav Toporovskyi <mtoporovskyy@gmail.com>
2023-12-08 21:43:00 +0200

    finished stage0-module4-conditions-task2

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
+        int equalFlip = 500;
 
+        if (from1UpTo1000 < equalFlip) {
+            System.out.println("Eagle");
+        } else {
+            System.out.println("Tail");
+        }
     }
 }
